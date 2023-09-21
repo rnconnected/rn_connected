344b4b442e31eca3608b252729c70f1bf91473f6
Chimaobim Dike Gabriel <89916820+moses-obi@users.noreply.github.com>
2023-09-22 00:35:57 +0100

    modified

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    STREAM_SECRET: z.string().min(1),
+    APPWRITE_MESSAGING_API_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_STREAM_KEY: z.string().min(1),
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID: z.string().min(1),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_STREAM_KEY: process.env.NEXT_PUBLIC_STREAM_KEY,
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+  },
+});
