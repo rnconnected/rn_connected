584221d34a89df56d472172edf55242ec30d1f9f
Chimaobim Dike Gabriel <89916820+moses-obi@users.noreply.github.com>
2023-10-30 18:36:43 +0100

    updated

--- src/app/dashboard/messages/page.tsx
+++ src/app/dashboard/messages/page.tsx
@@ -2,27 +2,27 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Login from "@/app/login/page";
 import Messages from "./components/Messages";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function Index() {
   let currentUser;
-  try {
-    const supabase = createServerComponentClient({ cookies });
-    let {
-      data: { user },
-    } = await supabase.auth.getUser();
 
-    if (!user) {
-      // Render the login page when the user is not logged in
-      return (
-        <div className="w-full h-screen flex flex-col items-center px-8 pt-8 custom">
-          <Login />
-        </div>
-      );
-    }
-    currentUser = user;
-  } catch (error) {
-    console.log("unable to get user");
+  // const supabase = createServerComponentClient({ cookies });
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+  let {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    // Render the login page when the user is not logged in
+    return (
+      <div className="w-full h-screen flex flex-col items-center px-8 pt-8 custom">
+        <Login />
+      </div>
+    );
   }
+  currentUser = user;
 
   return (
     <div className="w-full h-screen flex flex-col items-center px-8 pt-8 custom">
